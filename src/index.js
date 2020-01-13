4521f009a5161c8c2b3d18b484de327510173795
Emmanuel Tweneboah <twentekghana@gmail.com>
2020-01-13 15:11:49 +0800

    Scroll page to top when rendered

--- src/index.js
+++ src/index.js
@@ -10,12 +10,15 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./app/layout/App";
 import { Provider } from "react-redux";
 import { configureStore } from "./redux/store/store";
+import ScrollToTop from "./app/common/util/ScrollToTop";
 
 const store = configureStore();
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <ScrollToTop>
+        <App />
+      </ScrollToTop>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
